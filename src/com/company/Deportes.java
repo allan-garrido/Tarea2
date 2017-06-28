8cc33d00436fb7a047039015f1dd10181874af34
Allan Garrido <allan-garrido@users.noreply.github.com>
2017-06-27 19:47:21 -0600

    Clase Deportes Actualizada
    propiedad conBalon con un listado terminado

--- src/com/company/Deportes.java
+++ src/com/company/Deportes.java
@@ -8,40 +8,36 @@ public class Deportes {
             "Balonpesado", "Bochas", "Bolos", "Bolos césped", "Bowling americano",
             "Mölkky","Críquet","Croquet","Billar","Billar francés","Billiar inglés",
             "Pool", "Billar", "Snooker","Ciclobol","Balón prisionero o quemadas",
-            "Cuatro cuadras", "Faustball","Flickerball
-            "Floorball
-            "Fútbol
-            "Fútbol sala
-            "Fúbtol indoor
-            "Fútbol australiano
-            "Fútbol playa
-            "Fútbol gaélico
-            "Fútbol gridiron
-            "Fútbol americano
-            "Fútbol canadiense
-            "Fútbol americano indoor
-            "Fútbol-tenis
-            "Fútbol de reglas internacionales
-            "Fútbol de carnaval
-            "Rugby
-            "Rugby 13
-            "Rugby union
-            "Rugby subacuático
-            "Rugby en silla de ruedas
-            "Fútbol subacuático
-            "Futsal
-            "Futvóley
-            "Golf
-            "Half-rubber
-            "Balonmano
-    "Balonmano americano
-    "Balonmano australiano
-    "Balonmano gaélico
-    Hooverball
-            Hurling
-    Cesta punta
-    Malabarismo
-            Juggling
-    Kikimbol
-    Korfbal}
+            "Cuatro cuadras", "Faustball","Flickerball", "Floorball", "Fútbol",
+            "Fútbol sala", "Fúbtol indoor", "Fútbol australiano",
+            "Fútbol playa",
+            "Fútbol gaélico",
+            "Fútbol gridiron",
+            "Fútbol americano",
+            "Fútbol canadiense",
+            "Fútbol americano indoor",
+            "Fútbol-tenis",
+            "Fútbol de reglas internacionales",
+            "Fútbol de carnaval",
+            "Rugby",
+            "Rugby 13",
+            "Rugby union",
+            "Rugby subacuático",
+            "Rugby en silla de ruedas",
+            "Fútbol subacuático",
+            "Futsal",
+            "Futvóley",
+            "Golf",
+            "Half-rubber",
+            "Balonmano",
+    "Balonmano americano",
+    "Balonmano australiano",
+    "Balonmano gaélico",
+    "Hooverball",
+            "Hurling",
+    "Cesta punta",
+    "Malabarismo",
+            "Juggling",
+    "Kikimbol",
+    "Korfbal"};
 }
