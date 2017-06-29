08fc11220bf70050d6c84daed4a179e6df84b6c4
Diego827 <dmarroquin827@gmail.com>
2017-06-28 18:00:24 -0600

    Corrección en el Main
    Por corrección en el apartado Main.

---
+++ src/com/company/Especialista.java
@@ -0,0 +1,19 @@
+package com.company;
+
+/**
+ * Created by allan on 27/06/2017.
+ */
+public class Especialista {
+    private String Nombre;
+
+    public Especialista() {
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String nombre) {
+        Nombre = nombre;
+    }
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,52 @@
 package com.company;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 	// write your code here
+        //IOException e = new IOException();
+        BufferedReader Guardar = new BufferedReader((new InputStreamReader(System.in)));
+        Especialista miEspecialista = new Especialista();
+        int CantidadDeportes, Contador=0;
 
-        System.out.println("ingrese un deporte: ");
+        System.out.println("Deportes con balon");
+        System.out.println("");
+        System.out.println("Por favor ingrese su nombre: ");
+
+        miEspecialista.setNombre(Guardar.readLine());
+
+        System.out.println("");
+        System.out.println("Bienvenido " + miEspecialista.getNombre() + ", usted es nuestro especialista en deportes.");
+        System.out.println("");
+        System.out.println("¿Cuantos deportes desea? ");
+        CantidadDeportes= Integer.parseInt(Guardar.readLine());
+        System.out.println("Ingreso: "+CantidadDeportes);
+
+        Deporte[] listadoDeporte = new Deporte[CantidadDeportes];
+
+        for (int i=0; i<CantidadDeportes; i++){
+            listadoDeporte[i] = new Deporte();
+            System.out.println("Ingrese el nombre de un deporte: ");
+            listadoDeporte[i].setNombre(Guardar.readLine());
+
+            String Respuesta;
+
+            System.out.println("¿"+listadoDeporte[0].getNombre()+" es un deporte en el que se usa balon (S/N)? ");
+            Respuesta = Guardar.readLine();
+
+            if ( Respuesta.equals("s") || Respuesta.equals("S") )
+                listadoDeporte[i].setConBalon(Boolean.TRUE);
+            }
+            System.out.println("Los deportes con balón son: ");
+         for (int i=CantidadDeportes-1; i>=0; i--) {
+             if (listadoDeporte[i].getConBalon() == Boolean.TRUE) {
+                 System.out.println(listadoDeporte[i].getNombre() + " " + listadoDeporte[i].getConBalon());
+             }
+         }
+        }
     }
-}
